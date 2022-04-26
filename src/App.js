b63612491437a7b285911f44b9b581d3b856843e
Evans Sitibekiso <jevacrism@gmail.com>
2022-04-26 19:41:19 +0200

    add information on app.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Calculator from './components/Calculator';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Calculator />
+      </div>
+    );
+  }
+}
+
+export default App;
