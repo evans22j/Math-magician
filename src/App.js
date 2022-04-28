b25e42e7b49058a8f9219b9b188934c8e9ec0816
Evans Sitibekiso <jevacrism@gmail.com>
2022-04-28 14:47:38 +0200

    fix linter errors

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@ import React from 'react';
 import './App.css';
 import Calculator from './components/calculator';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <Calculator />
+  </div>
+);
 
 export default App;
