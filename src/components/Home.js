53ed8a5fa77f3aa90bfd991ac1a74b2374756730
Evans Sitibekiso <jevacrism@gmail.com>
2022-05-04 19:58:52 +0200

    create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Home = () => (
+  <div style={{
+    width: '80%',
+    margin: '1% auto',
+  }}
+  >
+    <h2>Welcome to our Math Magician Page</h2>
+    <p>
+      laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
+      architectincididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+      officia deserunt mollit anim id est laborum.squam est, qui dolorem ipsum quia dolor sit 
+      amet, consectetur, adipisci velit,sed quia non numquam eius modi tempora incidunt ut la
+      Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,
+      nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure
+      reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur,
+      vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.
+    </p>
+  </div>
+);
+
+export default Home;
