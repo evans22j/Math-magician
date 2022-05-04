83a3eab49066a72356d2082ae75f187cfd35f8a7
Evans Sitibekiso <jevacrism@gmail.com>
2022-05-04 22:28:06 +0200

    add information on app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Calculator from './components/calculator';
+import Calculator from './components/Calculator';
+import Header from './components/Header';
+import Home from './components/Home';
+import Quote from './components/Quote';
 
-const App = () => (
-  <div>
-    <Calculator />
-  </div>
-);
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="calculator" element={<Calculator />} />
+          <Route path="quote" element={<Quote />} />
+        </Routes>
+      </div>
+    );
+  }
+}
 
 export default App;
